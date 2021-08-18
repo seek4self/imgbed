e6653a69caaf7ad6f2d1f1057ed0112c86ff81a8
mazhuang <z.ma@bmi-tech.cn>
2021-08-18 15:27:13 +0800

    add ffmpeg img

--- img2link/main.go
+++ img2link/main.go
@@ -1,7 +1,7 @@
 /**************************************
  * @Author: mazhuang
  * @Date: 2021-08-16 16:35:00
- * @LastEditTime: 2021-08-17 11:40:58
+ * @LastEditTime: 2021-08-17 15:35:29
  * @Description:
  **************************************/
 
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	var cmd link
-	flag.StringVar(&cmd.domain, "d", "github", "Image link domain, choose 'cdn' to use cdn acceleration")
+	flag.StringVar(&cmd.domain, "d", "github", "Image link domain, choose 'cdn' to use jsDelivr CDN acceleration")
 	flag.StringVar(&cmd.style, "s", "md", "Image link style, 'md' for markdown, 'url' for http")
 	flag.StringVar(&cmd.target, "t", ".", "The path or folder of the target image ")
 	flag.Parse()
